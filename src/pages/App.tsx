d29c9bf466c43748479a2d74190016edb2590ce4
rilton bispo <riltondev@gmail.com>
2023-09-13 19:53:06 -0300

    style: app home

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import "./app.scss";
 import Banner from "../components/Banner/Banner";
+import Header from "../partials/Header/Header";
 
 function App() {
   return (
-    <>
+    <div className="home">
+      <Header></Header>
       <Banner></Banner>
-    </>
+    </div>
   );
 }
 
