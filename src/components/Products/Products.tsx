d43a784e5a0091c24c432c3cb9011d8eda2d9437
rilton bispo <riltondev@gmail.com>
2023-09-14 16:40:06 -0300

    feat: create products section

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,47 @@
+import Separator from "../Separator/Separator";
+import { getProducts } from "../../services/productsApi";
+import { useEffect, useState } from "react";
+import { ProductType } from "../../types/productType";
+import Card from "../Card/Card";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "./style.scss";
+
+const Products = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getProducts();
+        setProducts(response.products);
+      } catch (err) {
+        console.log("ERRO!", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="products__container">
+      <div className="products__content">
+        <Separator title="Produtos relacionados"></Separator>
+        <div className="products__swiper">
+          <Swiper slidesPerView={4} navigation spaceBetween={30}>
+            {products.map((product, index) => (
+              <SwiperSlide key={index} className="products__slides">
+                <Card photo={product.photo} price={product.price} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
