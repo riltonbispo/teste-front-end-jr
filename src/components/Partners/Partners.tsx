db727b3e7d73261eb67a70998dd8b0dd316dab1f
rilton bispo <riltondev@gmail.com>
2023-09-14 17:16:52 -0300

    feat: create partners

---
+++ src/components/Partners/Partners.tsx
@@ -0,0 +1,26 @@
+import "./style.scss";
+
+const Partners = () => {
+  return (
+    <div className="partners__container">
+      <div className="partners__content">
+        <div className="partners__item">
+          <h4 className="partner__title">Parceiros</h4>
+          <p className="partner__description">
+            Lorem ipsum dolor sit amet, consectetur
+          </p>
+          <button className="partner__button">Confira</button>
+        </div>
+        <div className="partners__item">
+          <h4 className="partner__title">Parceiros</h4>
+          <p className="partner__description">
+            Lorem ipsum dolor sit amet, consectetur
+          </p>
+          <button className="partner__button">Confira</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
