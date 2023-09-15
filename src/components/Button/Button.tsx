59ca0e5394214cee8847d8b947cc64ce0248079b
rilton bispo <riltondev@gmail.com>
2023-09-15 12:40:39 -0300

    feat: add props onClick button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,15 @@ type Props = {
   label: string;
   bold?: boolean;
   type: "extra-large" | "large" | "medium" | "small";
+  onClick?: () => void;
 };
 
 const Button = ({ ...props }: Props) => {
   return (
-    <button className={`button ${props.bold && "bold"} ${props.type}`}>
+    <button
+      className={`button ${props.bold && "bold"} ${props.type}`}
+      onClick={props.onClick}
+    >
       {props.label}
     </button>
   );
